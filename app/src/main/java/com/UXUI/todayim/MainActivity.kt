025590f7ed815179657a658ab70f0c1bd49acdec
zae-hyeong <p0138@naver.com>
2022-06-17 12:15:08 +0900

    [fix] test Activity Err

--- app/src/main/java/com/UXUI/todayim/MainActivity.kt
+++ app/src/main/java/com/UXUI/todayim/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.UXUI.todayim
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.UXUI.todayim.databinding.ActivityMainBinding
 import com.UXUI.todayim.ui.analyze.AnalyzeFragment
 import com.UXUI.todayim.ui.home.HomeFragment
